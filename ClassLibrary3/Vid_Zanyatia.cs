3aa98f0f4285bf520c034a8c5526cea24e9801f2
MatveyMM <148620193+MatveyMM@users.noreply.github.com>
2024-05-02 12:50:59 +0300

    Update Vid_Zanyatia.cs

--- ClassLibrary3/Vid_Zanyatia.cs
+++ ClassLibrary3/Vid_Zanyatia.cs
@@ -5,8 +5,6 @@ global using global::System.Linq;
 global using global::System.Net.Http;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
-
-
 namespace Vid_Zanyatia
 {
     public class Vid
